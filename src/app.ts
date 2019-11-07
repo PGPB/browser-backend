5a9f10912f0b1163273990480525f360cbae3394
Amaimersion <amatsukine@gmail.com>
2019-11-07 16:15:01 +0300

    Add test app.ts

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+/* ENV Variables. */
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+
+/* Express app. */
+
+import * as express from "express";
+
+const app = express();
+
+
+/* Middlewares. */
+
+import * as helmet from "helmet";
+import * as lusca from "lusca";
+import * as compression from "compression";
+import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
+
+app.use(helmet());
+app.use(lusca.xssProtection(true));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+
+
+/* Express app. */
+
+export default app;
