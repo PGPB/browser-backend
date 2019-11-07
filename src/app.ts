4b1f58a5f119e5cf51aba9dfd82f7054d7c305c3
Amaimersion <amatsukine@gmail.com>
2019-11-07 16:30:13 +0300

    Refactoring of app.ts

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,14 @@ app.use(helmet());
 app.use(lusca.xssProtection(true));
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(morgan(
+    process.env.NODE_ENV === "production" ?
+    "combined" :
+    "dev"
+));
 
 
 /* Express app. */
